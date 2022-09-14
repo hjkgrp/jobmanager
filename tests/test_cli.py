29fe6a6283f59080fc81f56c090299606f9d2943
Ralf Meyer <meyer.ralf@yahoo.com>
2022-09-14 17:21:24 -0400

    Add missing output capture

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,7 @@ import subprocess
 def test_cli_call():
     # TODO: there might be a more elegant way of checking that the entry
     # point is registered correctly.
-    output = subprocess.run(["jobmanager"])
+    output = subprocess.run(["jobmanager"], capture_output=True)
     # This is expected to fail because the jobmanager does not
-    # recognize the machine it is running on which we check using
+    # recognize the machine it is running on:
     assert "ValueError: Machine Unknown to Job Manager" in output.stderr
