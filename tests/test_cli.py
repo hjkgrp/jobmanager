e923b94e2d41c94a1407c6d54e2e65a24baf5c44
Ralf Meyer <meyer.ralf@yahoo.com>
2022-09-14 16:52:18 -0400

    Testing different test versions

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,6 @@ import pytest
 
 
 def test_cli_call():
-    # Expecting an "unknown machine" value error if the command line
-    # script is working correctly
-    with pytest.raises(ValueError):
-        subprocess.run(['jobmanager'])
+    output = subprocess.run(['jobmanager'])
+    print(output)
+    assert False
