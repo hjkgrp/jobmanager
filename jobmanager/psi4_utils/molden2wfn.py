55119d3a67b95600a6180e68f5c44e9c9ad0b282
aarongarrison <aaron_g@mit.edu>
2024-06-10 16:10:41 -0400

    Documenting TC to Psi4 conversion functionality.

--- jobmanager/psi4_utils/molden2wfn.py
+++ jobmanager/psi4_utils/molden2wfn.py
@@ -8,9 +8,13 @@ class Cartesian:
 
     def internal_ao_mapping(self, shell_type):
         """
-        Informs how to shift the individual AOs within a set with the same n, l
+        Informs how to shift the individual AOs within a subshell (same n, l)
         Read as i:j where i is the position of the original (TC) AO,
         and j is the position of the AO for Psi4.
+        e.g., for p (shell_type==1), the px, py, and pz orderings are the same,
+        so a zero shift for 3 of the AOs.
+        Note: each subshell is not a full subshell, but rather a basis function for that subshell.
+        So, there could be multiple calls of this function for one subshell for multiple-zeta.
         """
         if abs(shell_type) == 0:
             return {0: 0}
@@ -24,17 +28,32 @@ class Cartesian:
 
     def shell_sequence_mapping(self, atom_shell_types):
         """
-        Informs how to shift the sequence of orbitals with same n, l
+        Informs how to shift the subshells (groups of AOs with same n, l)
         Read as i:j where i is the position of the original (TC) set of AOs
         and j is the position of the set of AOs in Psi4
+        Note: tells how to shift the AOs in the subshell, not how the subshells related.
+        The number of elements in the dictionary is not the number of subshells, but rather the
+        number of basis functions used to represent the atom.
+        See below for examples.
         """
         # 6-31g* on C, O, N...
+        #Molden is 1s(6), 2s(3), 2s(1), 2p(3), 2p(1), d
+        #Generates 1,     1,     1,     3,     3,     6 orbitals
+        #Psi4 is   1s(6), 2s(3), 2p(3), 2s(1), 2p(1), d
+        #Generates 1,     1,     3,     1,     3,     6 orbitals
+        #So, need to shift 2s(1) orbitals right 3, 2p(3) orbitals left one.
         if "-".join([str(x) for x in atom_shell_types]) == "-".join([str(x) for x in [0, 0, 0, 1, 1, 2]]):
             return {0: 0, 1: 0, 2: 3, 3: -1, 4: 0, 5: 0}  # for TC
         # 6-31g on C, O, N...
+        #Same as above but with no d function.
         elif "-".join([str(x) for x in atom_shell_types]) == "-".join([str(x) for x in [0, 0, 0, 1, 1]]):
             return {0: 0, 1: 0, 2: 3, 3: -1, 4: 0}
         # 6-31g* on P, S, Cl...
+        #Molden is 1s(6), 2s(6), 3s(3), 3s(1), 2p(6), 3p(3), 3p(1), d
+        #Generates 1,     1,     1,     1,     3,     3,     3,     6 orbitals
+        #Psi4   is 1s(6), 2s(6), 2p(6), 3s(3), 3p(3), 3s(1), 3p(1), d
+        #Generates 1,     1,     3,     1,     3,     1,     3,     6 orbitals
+        #So, need to shift 3s(3) orbitals right 3, 3s(1) right 6, 2p(6) left 2, 3p(3) left 1.
         elif "-".join([str(x) for x in atom_shell_types]) == "-".join([str(x) for x in [0, 0, 0, 0, 1, 1, 1, 2]]):
             return {0: 0, 1: 0, 2: 3, 3: 6, 4: -2, 5: -1, 6: 0, 7: 0}
         # 6-31g* on metals
@@ -49,7 +68,7 @@ class Cartesian:
 
     def mocoeff_c2s(self, mocoeffs_c, shell_types):
         '''
-        Psi4 normalizes basis functions according to:
+        Psi4 normalizes basis functions according to (for d-orbitals):
         For Cartesian functions we need to add a basis function normalization constant of
         //      _______________________________
         //     / (2lx-1)!! (2ly-1)!! (2lz-1)!!
@@ -59,7 +78,12 @@ class Cartesian:
         
         d_conv, d_conv_inv: conversion metrix.
         source: https://github.com/psi4/psi4/blob/master/psi4/src/psi4/libmints/writer.cc, FCHKWriter
+
+        Converts the molecular orbital coefficients
+        (as represented by number of basis functions, where each s is counted once, p thrice, d six times)
+        from TeraChem normalization to Psi4 normalization, by the above scale factor
         '''  # noqa W605
+        #to normalize as above for the d-orbitals
         pf1 = 1.0
         pf2 = np.sqrt(1.0 / 3.0)
         d_conv = np.array([
@@ -94,6 +118,11 @@ class Cartesian:
 
 
     def mocoeff_mapping(self, d_molden, mapping, key='alpha'):
+        """
+        Maps the molecular orbital coefficients (again represented with number of basis functions
+        with s counted once, p thrice, and d six times) in TeraChem to that in Psi4, using the
+        mapping developed in mapping.
+        """
         coeff = np.copy(d_molden['orb_%s_coeffs' % key])
         coeff = self.mocoeff_c2s(coeff, d_molden['obasis']['shell_types'])
         mooeff_psi4 = np.zeros(shape=coeff.shape)
@@ -105,26 +134,29 @@ class Cartesian:
 
     def tcmolden2psi4wfn_ao_mapping(self, d_molden, restricted=False):
         """
-        mapping returns a dictionary mapping the AOs in the TC molden to the AOs in a Psi4 wfn.
+        mapping returns a dictionary mapping the MO coefficients in the TC molden to those in a Psi4 wfn.
+        Ca and Cb are the coefficients for the alpha and beta orbitals, again read from the molden.
         """
         mapping = {}
-        start = 0
-        ao_start = 0
-        for atom_i in np.unique(d_molden['obasis']['shell_map']):
-            num_shell = np.count_nonzero(d_molden['obasis']['shell_map'] == atom_i)
-            atom_shell_types = d_molden['obasis']['shell_types'][start: start+num_shell]
+        start = 0 #iterate over the number of basis functions
+        ao_start = 0 #iterate over the atomic orbitals (1 for s basis, 3 for p, 6 for d)
+        for atom_i in np.unique(d_molden['obasis']['shell_map']): #for each atom in the molecule
+            num_shell = np.count_nonzero(d_molden['obasis']['shell_map'] == atom_i) #get the number of basis functions associated with the atom
+            atom_shell_types = d_molden['obasis']['shell_types'][start: start+num_shell] #get the identity of associated shells
             start += num_shell
-            shell_mapping = self.shell_sequence_mapping(atom_shell_types)
-            print(shell_mapping)
-            for ii in range(len(shell_mapping.keys())):
-                shell_type = atom_shell_types[ii]
-                ao_mapping_local = self.internal_ao_mapping(shell_type)
-                print(ii, shell_type, ao_mapping_local)
-                for jj in range(len(ao_mapping_local.keys())):
+            shell_mapping = self.shell_sequence_mapping(atom_shell_types) #get a dictionary mapping how shells are ordered for the atom
+            #print(shell_mapping)
+            for ii in range(len(shell_mapping.keys())): #for each basis function
+                shell_type = atom_shell_types[ii] #get the shell type
+                ao_mapping_local = self.internal_ao_mapping(shell_type) #reorder AOs in the shell as needed
+                #print(ii, shell_type, ao_mapping_local)
+                for jj in range(len(ao_mapping_local.keys())): #for each AO
+                    #map the MO position to what it would be in Psi4
+                    #adjust the MO by the rearrangement in the basis function, and by rearrangements in basis functions for each atom
                     mapping.update(
                         {ao_start: ao_start+ao_mapping_local[jj]+shell_mapping[ii]})
                     ao_start += 1
-                print(mapping)
+        #assign the coefficients
         Ca = self.mocoeff_mapping(d_molden, mapping, key='alpha')
         if not restricted:
             Cb = self.mocoeff_mapping(d_molden, mapping, key='beta')
