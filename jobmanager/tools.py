9eb4e76edf098e6100047cde3bdbc648ededcdbf
dbkchu <46388253+dbkchu@users.noreply.github.com>
2024-06-18 13:14:37 -0400

    add supercloud to another list

--- jobmanager/tools.py
+++ jobmanager/tools.py
@@ -290,7 +290,7 @@ def list_active_jobs(ids=False, home_directory=False, parse_bundles=False):
     try:
         if get_machine() == 'gibraltar':
             job_report.lines = call_bash("qstat -r")
-        elif get_machine() in ['comet', 'bridges','expanse']:
+        elif get_machine() in ['comet', 'bridges','expanse','supercloud']:
             job_report.lines = call_bash('squeue -o "%.18i %.9P %.50j %.8u %.2t %.10M %.6D %R" -u '+get_username(),
                                          version=2)
         else:
