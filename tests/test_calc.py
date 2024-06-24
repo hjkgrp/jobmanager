a28e87bb5cb8613831916a24d9b93f152a6c1bb7
Daniel <46388253+dbkchu@users.noreply.github.com>
2024-06-24 12:16:07 -0400

    Correct reference in test

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,7 +1,7 @@
-from jobmanager.Classes.calc import Calc
+from jobmanager.Classes.calc import TerachemCalc
 
 def test_read_input(resource_path_root):
-    sample = Calc()
+    sample = TerachemCalc()
     sample.read_input(str(resource_path_root / 'inputs' / 'sample_input.in'))
     assert (sample.run == 'minimize' and sample.spinmult == '5' and
         sample.charge == '3' and sample.method == 'B3LYP' and
